ff532e18c19101ba5f3d42a65493c1b756419c9c
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-11-17 15:07:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+
+class b;
+
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    float a,b,x,y,z,c,d,e,f,g,h;
+    cin >> a >> b >> x >> y;
+
+            if (x<0 and y>0) {
+                c=(a*x);
+                        d=(b*y);
+                                z=c-d;
+    } else if (x>=0 and y<=0) {
+                e=(a*x*x);
+                        f=(b*y);
+                     z=e-f;
+            } else {
+                g=(a*x);
+                        h=(b*y*y);
+                z=g+h;
+            }
+
+    cout<< z;
+    return 0;
+
 }
